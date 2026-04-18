8f4544a6f94946c77f0fc2984419ee7725e030fa
R Aditya Murty <adityamurty2006@gmail.com>
2026-04-18 10:44:34 +0530

    Problem 3

---
+++ while loop/Problem3.cpp
@@ -0,0 +1,14 @@
+// Print Alphabet from a to z
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch = 'a';
+    while(ch<='z'){
+        cout<< ch << " ";
+        ch++;
+    }
+    cout<< endl;
+    return 0;
+}
