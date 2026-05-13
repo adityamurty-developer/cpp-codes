62e30f94337fbb156d38f684370c6fc5b560e8d5
R Aditya Murty <adityamurty2006@gmail.com>
2026-05-13 11:56:49 +0530

    Problem 3

---
+++ Array - Practice/problem3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5];
+    for(int i=0; i<5; i++){
+        cout<<"Enter array " << i << ": ";
+        cin>> arr[i];
+    }
+    
+    cout<<"Array is: ";
+
+    for(int i=0; i<5; i++){
+        cout<< arr[i] << " ";
+    }
+    cout<<endl;
+    int sum = 0;
+    for(int i=0; i<5; i++){
+        sum+=arr[i];
+    }
+    cout<<"Sum of array is: " << sum << endl;
+    float avg = (float)sum/5;
+    cout<<"Average is: " << avg;
+
+    return 0;
+}
