49f51bbc480405dc9f5d6df9860cc2149f91e54d
R Aditya Murty <adityamurty2006@gmail.com>
2026-05-13 11:39:29 +0530

    Problem 1

---
+++ Array - Practice/Problem1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n[5] = {1, 3, 8, 2, 5};
+    int max = 0;
+    for(int i = 0; i<5; i++){
+        if(max<n[i]){
+            max = n[i];
+        }
+    }
+    cout<< "max is: " << max << endl;
+    return 0;
+}
