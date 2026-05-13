d149b67d4702c29f0d42a526081ebc96ac958d3b
R Aditya Murty <adityamurty2006@gmail.com>
2026-05-13 11:46:31 +0530

    Problem 2

--- Array - Practice/Problem1.cpp
+++ Array - Practice/Problem1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     int n[5] = {1, 3, 8, 2, 5};
-    int max = 0;
+    int max = n[0];
     for(int i = 0; i<5; i++){
         if(max<n[i]){
             max = n[i];
---
+++ Array - Practice/Problem2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n[5] = {2, 5, 13, 1, 9};
+    int min = n[0];
+    for(int i = 0; i<5; i++){
+        if(min>n[i]){
+            min = n[i];
+        }
+    }
+    cout<<"Minimum number in array is: " << min << endl;
+    return 0;
+}
